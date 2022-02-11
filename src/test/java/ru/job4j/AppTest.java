b1e49acb7807385e4fbbc5e0418ab32bf7b27e35
Admin <shaidurov.ilha@yandex.ru>
2022-02-11 19:17:45 +0300

    0.1 Бот

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class AppTest extends TestCase {
-    public AppTest( String testName ) {
+    public AppTest(String testName) {
         super(testName);
     }
 
