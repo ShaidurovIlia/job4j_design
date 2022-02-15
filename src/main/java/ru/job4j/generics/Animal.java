99d41d89a9ff1c91516b854907b87b75dea5e2e2
Admin <shaidurov.ilha@yandex.ru>
2022-02-15 11:57:36 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.generics;
+
+public class Animal {
+    private String name = "Животные";
+
+    @Override
+    public String toString() {
+        return "Animal{"
+                + "name='" + name
+                + '\'' + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,12 @@
+package ru.job4j.generics;
+
+public class Predator extends Animal {
+    private String name = "Хищники";
+
+    @Override
+    public String toString() {
+        return "Predator{"
+                + "name='" + name
+                + '\'' + '}';
+    }
+}
