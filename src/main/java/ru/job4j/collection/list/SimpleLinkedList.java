322b547072d9dec125ebc5e2a0f9bb7a90f41b77
Admin <shaidurov.ilha@yandex.ru>
2022-02-23 23:33:26 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,71 @@
+package ru.job4j.collection.list;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(last, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> rsl = first;
+        for (int i = 0; i < index; i++) {
+            rsl.item = rsl.next.item;
+        }
+        return rsl.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> nextNode = first;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return nextNode != null;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                } else if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E rsl = nextNode.item;
+                nextNode = nextNode.next;
+                return rsl;
+            }
+        };
+    }
+
+    public static class Node<E> {
+        private E item;
+        private Node<E> pref;
+        private Node<E> next;
+
+        public Node(Node<E> pref, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.pref = pref;
+        }
+    }
+}
