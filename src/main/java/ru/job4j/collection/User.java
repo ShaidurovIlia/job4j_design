ff2cbd67d13ffde9c73672939bb1753ac6e5b918
Admin <shaidurov.ilha@yandex.ru>
2022-04-07 23:49:57 +0300

    2. Без переопределения equals и hashCode [#1005 #285867]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -13,42 +13,14 @@ public class User {
         this.birthday = birthday;
     }
 
-    @Override
-    public String toString() {
-        String date = String.format("%04d.%02d.%02d%n",
-                birthday.get(Calendar.YEAR),
-                birthday.get(Calendar.MONTH) + 1,
-                birthday.get(Calendar.DAY_OF_MONTH));
-        return "Name:" + name
-                + " Children:" + children
-                + " Birthday:" + date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return children == user.children
-                && name.equals(user.name)
-                && birthday.equals(user.birthday);
-    }
-
-    @Override
-    public int hashCode() {
-        return 0;
-    }
-
     public static void main(String[] args) {
 
         Calendar calendar = new GregorianCalendar(2000, Calendar.APRIL, 12);
         User  first = new User("Ivan", 12, calendar);
         User second = new User("Ivan", 12, calendar);
+
         Map<User, Object> userObjectMap = new HashMap<>();
+
         userObjectMap.put(first, new Object());
         userObjectMap.put(second, new Object());
 
