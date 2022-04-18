2290d5f6468b7c5ee3e6dcca0d5d892a4145e311
Admin <shaidurov.ilha@yandex.ru>
2022-04-18 20:28:23 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/collection/tree/SimpleTree.java
+++ src/main/java/ru/job4j/collection/tree/SimpleTree.java
@@ -3,12 +3,13 @@ package ru.job4j.collection.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
     public SimpleTree(final E root) {
-        this.root = new Node<E>(root);
+        this.root = new Node<>(root);
     }
 
     @Override
@@ -21,14 +22,22 @@ public class SimpleTree<E> implements Tree<E> {
         return result;
     }
 
+    public boolean isBinary() {
+        return findByPredicate(x -> x.getChildren().size() > 2).isEmpty();
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(el -> el.getValue().equals(value));
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
