8263ce98544f0e54071e13236852ae67d5631112
Admin <shaidurov.ilha@yandex.ru>
2022-04-18 20:53:18 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.rmi.server.ExportException;
+
+public class ResultFile {
+    public static void main(String[] args) throws IOException {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    out.write(((i * j + "|").getBytes()));
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (ExportException e) {
+            e.printStackTrace();
+        }
+    }
+}
