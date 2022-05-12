2ca89d4b41ce82175933799dcf2c58c7f54b0ec0
Admin <shaidurov.ilha@yandex.ru>
2022-05-12 22:26:57 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analisy.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analisy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+            PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                boolean trouble = false;
+                String line = reader.readLine();
+                while (line != null) {
+                    String[] lineSplit = line.split(" ");
+                    if (("400".equals(lineSplit[0])
+                            && !trouble) || ("500".equals(lineSplit[0]) && !trouble)) {
+                        out.print(lineSplit[1] + ";");
+                        trouble = true;
+                    } else if (trouble && !"400".equals(lineSplit[0])
+                            && !"500".equals(lineSplit[0])) {
+                        out.println(lineSplit[1] + ";");
+                    }
+                    line = reader.readLine();
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analisy analisy = new Analisy();
+        analisy.unavailable("./server_source.txt",
+                "./target.txt");
+        analisy.unavailable("./server_source_sec.txt",
+                "./target_sec.txt");
+        try (BufferedReader out = new BufferedReader(
+                new FileReader("./target.txt"));
+            BufferedReader outSecond = new BufferedReader(
+                    new FileReader("./target_sec.txt"))) {
+                out.lines().forEach(System.out::println);
+            System.out.println();
+            outSecond.lines().forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
