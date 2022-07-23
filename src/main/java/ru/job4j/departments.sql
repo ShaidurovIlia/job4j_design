9c655c1f2f2f58b0286750198dc530f1778d8f2f
Admin <shaidurov.ilha@yandex.ru>
2022-07-23 13:51:47 +0300

    0. Join [#6862]

---
+++ src/main/java/ru/job4j/departments.sql
@@ -0,0 +1,44 @@
+create table departments
+(
+	id serial primary key,
+	name varchar(255)
+);
+create table employees
+(
+id serial primary key,
+name varchar(255),
+department_id int references departments(id)
+);
+
+insert into departments (name) values
+('Devolopment'),('Prodaction'),('Management'),('Accounting');
+
+insert into employees(name, department_id) values('Ivan', 1), ('Egor', 2);
+insert into employees(name, department_id) values('Olga', 3),('Artur', 1); 
+insert into employees(name, department_id) values('Sveta', 2), ('Julia', null);
+
+select * from departments d 
+left join employees e 
+on e.department_id=d.id
+where e.id is null;
+
+select * from employees e
+left join departments d
+on e.department_id=d.id
+where d.id is not null;
+
+create table teens 
+(
+id serial primary key,
+name varchar(255),
+gender varchar(255)
+);
+
+insert into teens(name, gender) values('Oleg', 'M'), ('Egor', 'M');
+insert into teens(name, gender) values('Katia', 'W'), ('Olga', 'W');
+insert into teens(name, gender) values('Petr', 'M'), ('Nika', 'W');
+
+select t1.name as Имя_M, t2.name as Имя_W 
+from teens t1 cross join  teens t2
+where t1.gender != t2.gender;
+
